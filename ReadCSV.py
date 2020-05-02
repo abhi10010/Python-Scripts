1a3388a5976d6ad0e362be254d6178c5507ced04
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-05-02 09:22:53 +0530

    Add files via upload

---
+++ ReadCSV.py
@@ -0,0 +1,12 @@
+import sqlite3
+conn = sqlite3.connect("New.db")
+print("Db opened!!")
+sql_command = "SELECT field21, Count(*) FROM orders GROUP BY field21 ORDER BY 2"
+print("SQL Command Running!")
+cursor = conn.execute(sql_command)
+for row in cursor:
+    print("STYLECODE = ")
+    print(row[0])
+    print("Count = ", row[1], "\n")
+conn.commit()
+conn.close()
