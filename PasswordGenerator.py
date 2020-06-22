0ccc5ea73aa5959bd90a6ab9b2cff2d4ccd09f1a
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-23 01:18:27 +0530

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,26 +1,27 @@
 import random
 
-uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXWZ"
-lowercase_letters = uppercase_letters.lower()
-digits = "1234567890"
-special_characters = ".,/?!@#$%&_=[]{}()"   #Add or delete characters as per the given acceptance criteria
+def Generate_Ultimate_Password(length = 16):
+  uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXWZ"
+  lowercase_letters = uppercase_letters.lower()
+  digits = "1234567890"
+  special_characters = ".,/?!@#$%&_=[]{}()"   #Add or delete characters as per the given acceptance criteria
 
-all_characters = uppercase_letters + lowercase_letters + digits + special_characters
+  all_characters = uppercase_letters + lowercase_letters + digits + special_characters
 
-length = 16                                 #Change the length according to the requirement
+  sub_length = length//4
 
-sub_length = length//4
+  password_uc = "".join(random.choices(uppercase_letters, k = sub_length)) #With Replacement
+  password_lc = "".join(random.choices(lowercase_letters, k = sub_length)) #With Replacement
+  password_d = "".join(random.choices(digits, k = sub_length)) #With Replacement
+  password_sc = "".join(random.choices(special_characters, k = sub_length)) #With Replacement
 
-password_uc = "".join(random.sample(uppercase_letters, sub_length))
-password_lc = "".join(random.sample(lowercase_letters, sub_length))
-password_d = "".join(random.sample(digits, sub_length))
-password_sc = "".join(random.sample(special_characters, sub_length))
+  if length != sub_length*4:
+    password_ac = "".join(random.choices(all_characters, k = length - (sub_length*4))) #With Replacement
+  else:
+    password_ac = ''
 
-if length != sub_length*4:
-  password_ac = "".join(random.sample(all_characters, length - (sub_length*4)))
-else:
-  password_ac = ''
+  password = "".join(random.sample(password_uc + password_lc + password_d + password_sc + password_ac, length)) #Without Replacement
+  
+  return password
 
-password = "".join(random.sample(password_uc + password_lc + password_d + password_sc + password_ac, length))
-
-print(password)
+print(Generate_Ultimate_Password(length = 15))
