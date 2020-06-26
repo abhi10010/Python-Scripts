2883750047a66f75554ae4928b012451785f9a76
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-26 16:44:17 +0530

    Update White2GreyBackground.py

--- White2GreyBackground/White2GreyBackground.py
+++ White2GreyBackground/White2GreyBackground.py
@@ -8,7 +8,7 @@ def changeBG():
     newData = []
     
     for item in datas:
-        if item[0] >= 220 and item[1] >= 220 and item[2] >= 220: # Background Color with Threshold of 35
+        if item[0] >= 220 and item[1] >= 220 and item[2] >= 220: # Background Color with threshold of 35
             newData.append((200, 201, 198, 255))                 # RGBA value for the new background color, use A = 0  for transparent background 
         else:
             newData.append(item)
